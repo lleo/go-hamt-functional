521bd6a53aa271618c96b6cbc2766f3fd62f2a4c
LLeo <lleoem@gmail.com>
2016-09-15 19:24:50 -0400

    moved bitCount64() from table.go to compressed_table.go

--- hamt64/compressed_table.go
+++ hamt64/compressed_table.go
@@ -249,3 +249,20 @@ func (t compressedTable) set(idx uint, nn nodeI) tableI {
 
 	return nt
 }
+
+//POPCNT Implementation
+// copied from https://github.com/jddixon/xlUtil_go/blob/master/popCount.go
+//  was MIT License
+
+const (
+	hexi_fives  = uint64(0x5555555555555555)
+	hexi_threes = uint64(0x3333333333333333)
+	hexi_ones   = uint64(0x0101010101010101)
+	hexi_fs     = uint64(0x0f0f0f0f0f0f0f0f)
+)
+
+func bitCount64(n uint64) uint {
+	n = n - ((n >> 1) & hexi_fives)
+	n = (n & hexi_threes) + ((n >> 2) & hexi_threes)
+	return uint((((n + (n >> 4)) & hexi_fs) * hexi_ones) >> 56)
+}
