250572ea647cc4f69fb92d9333a776a752e7ec7b
LLeo <lleoem@gmail.com>
2017-02-08 04:27:33 -0500

    - changed to function hamt testing - added Benchmark functions

--- hamt32/hamt32_test.go
+++ hamt32/hamt32_test.go
@@ -2,39 +2,22 @@ package hamt32_test
 
 import (
 	"log"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/lleo/go-hamt-functional/hamt32"
+	"github.com/lleo/go-hamt/stringkey"
+	"github.com/lleo/stringutil"
 )
-
-func TestHamt32NewFullTablesOnly(t *testing.T) {
-	var h = hamt32.New(hamt32.FullTablesOnly)
-	if h == nil {
-		t.Fatal("no new Hamt struct")
-	}
-}
-
-func TestHamt32NewCompTablesOnly(t *testing.T) {
-	var h = hamt32.New(hamt32.CompTablesOnly)
-	if h == nil {
-		t.Fatal("no new Hamt struct")
-	}
-}
-
-func TestHamt32NewHybridTables(t *testing.T) {
-	var h = hamt32.New(hamt32.HybridTables)
-	if h == nil {
-		t.Fatal("no new Hamt struct")
-	}
-}
 
 func TestBuildHamt32(t *testing.T) {
 	log.Println("TestBuildHamt32:")
-	var h = hamt32.New(TableOption)
+	var h = hamt32.Hamt{}
 
 	var added bool
 	for _, kv := range hugeKvs {
-		*h, added = h.Put(kv.Key, kv.Val)
+		h, added = h.Put(kv.Key, kv.Val)
 		if !added {
 			t.Fatalf("failed to h.Put(%s, %v)", kv.Key, kv.Val)
 		}
@@ -44,7 +27,7 @@ func TestBuildHamt32(t *testing.T) {
 	var val interface{}
 	var removed bool
 	for _, kv := range hugeKvs {
-		*h, val, removed = h.Del(kv.Key)
+		h, val, removed = h.Del(kv.Key)
 		if !removed {
 			t.Fatalf("failed to h.Del(%s)", kv.Key)
 		}
@@ -58,4 +41,74 @@ func TestBuildHamt32(t *testing.T) {
 	if !h.IsEmpty() {
 		t.Fatalf("!h.IsEmpty()")
 	}
+}
+
+func BenchmarkHamt32Get(b *testing.B) {
+	log.Printf("BenchmarkHamt32Get: b.N=%d", b.N)
+
+	for i := 0; i < b.N; i++ {
+		var j = int(rand.Int31()) % numHugeKvs
+		var key = hugeKvs[j].Key
+		var val0 = hugeKvs[j].Val
+		var val, found = LookupHamt32.Get(key)
+		if !found {
+			b.Fatalf("H.Get(%s) not found", key)
+		}
+		if val != val0 {
+			b.Fatalf("val,%v != hugeKvs[%d].val,%v", val, j, val0)
+			//b.Fatalf("val,%v != midKvs[%d].val,%v", val, j, val0)
+		}
+	}
+}
+
+func BenchmarkHamt32Put(b *testing.B) {
+	log.Printf("BenchmarkHamt32Put: b.N=%d", b.N)
+
+	var h = hamt32.Hamt{}
+	var s = "aaa"
+	for i := 0; i < b.N; i++ {
+		key := stringkey.New(s)
+		val := i + 1
+		h, _ = h.Put(key, val)
+		s = stringutil.DigitalInc(s)
+	}
+}
+
+func BenchmarkHamt32Del(b *testing.B) {
+	log.Printf("BenchmarkHamt32Del: b.N=%d", b.N)
+
+	// We rebuild the DeleteHamt32 datastructure because this Benchmark will probably be
+	// rereun with different b.N values to get a better/more-accurate benchmark.
+
+	StartTime["BenchmarkHamt32Del:rebuildDeleteHamt32"] = time.Now()
+
+	//rebuildDeleteHamt32(hugeKvs)
+	var h = DeleteHamt32 //its functional and I am an idiot
+
+	RunTime["BenchmarkHamt32Del:rebuildDeleteHamt"] = time.Since(StartTime["BenchmarkHamt32Del:rebuildDeleteHamt32"])
+
+	b.ResetTimer()
+
+	StartTime["run BenchmarkHamt32Del"] = time.Now()
+	for i := 0; i < b.N; i++ {
+		kv := hugeKvs[i]
+		key := kv.Key
+		val := kv.Val
+
+		var v interface{}
+		var deleted bool
+		h, v, deleted = h.Del(key)
+		if !deleted {
+			b.Fatalf("failed to find and delet key=%s", key)
+		}
+		if v != val {
+			b.Fatalf("deleted key=%s but the value found was wrong v=%d, expected val=%d", key, v, val)
+		}
+	}
+
+	if h.IsEmpty() {
+		b.Fatal("DeleteHamt32.IsEmpty() => true; hence this wasn't a valid benchmark")
+	}
+
+	RunTime["run BenchmarkHamt32Del"] = time.Since(StartTime["run BenchmarkHamt32Del"])
 }
