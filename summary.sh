102ed022eff77efd7dc74aa56d7b91d69bddc1ab
LLeo <lleoem@gmail.com>
2017-04-02 05:38:28 -0400

    added benchcmp

--- summary.sh
+++ summary.sh
@@ -4,8 +4,12 @@ echo "Builtin Map" vs "Functional Hamt32 w/HybridTables"
 benchcmp map.b hamt32-hybrid.b
 
 echo
-echo "Builtin Map" vs "Functional Hamt64 w/CompressedTables"
-benchcmp map.b hamt64-comp.b
+echo "Builtin Map" vs "Functional Hamt32 w/CompressedTables"
+benchcmp map.b hamt32-comp.b
+
+echo
+echo "Builtin Map" vs "Functional Hamt32 w/FullTables"
+benchcmp map.b hamt32-full.b
 
 echo
 echo "Functional Hamt32 w/HybridTables" vs "Functional Hamt32 w/CompressedTables"
