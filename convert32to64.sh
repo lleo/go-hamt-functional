067a13f698e9056fff8e49352f168f7a68702396
LLeo <lleoem@gmail.com>
2017-04-11 23:55:54 -0400

    added some more search/replace patterns

--- convert32to64.sh
+++ convert32to64.sh
@@ -2,10 +2,17 @@
 
 for f in "$@"; do
 	echo $f
+
 	echo "perl -pi -e 's/32/64/g' $f"
 	perl -pi -e 's/32/64/g' $f
+
 	echo "perl -pi -e 's/30/60/g' $f"
 	perl -pi -e 's/30/60/g' $f
-	#echo "perl -pi -e 's/six/ten/g'"
-done
 
+	echo "perl -pi -e 's/\bsix\b/ten/g'"
+	perl -pi -e 's/\bsix\b/ten/g' $f
+
+	echo "perl -pi -e 's/\b5\b/6/g'"
+	perl -pi -e 's/\b5\b/6/g' $f
+
+done
